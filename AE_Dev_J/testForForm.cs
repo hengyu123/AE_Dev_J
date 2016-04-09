ebcd51674164799b3d05ec57dfc4037115266d10
hengyu123 <hengyu.hengyu@163.com>
2016-04-09 16:33:22 +0800

    重构代码
    重新组织代码结构，添加了一些新功能的框架。

--- AE_Dev_J/testForForm.cs
+++ AE_Dev_J/testForForm.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using DevExpress.LookAndFeel;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Windows.Forms;
+//using DevExpress.LookAndFeel;
 
-namespace AE_Dev_J
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+//namespace AE_Dev_J
+//{
+//    static class Program
+//    {
+//        /// <summary>
+//        /// The main entry point for the application.
+//        /// </summary>
+//        [STAThread]
+//        static void Main()
+//        {
+//            Application.EnableVisualStyles();
+//            Application.SetCompatibleTextRenderingDefault(false);
 
-            DevExpress.Skins.SkinManager.EnableFormSkins();
-            DevExpress.UserSkins.BonusSkins.Register();
-            UserLookAndFeel.Default.SetSkinStyle("Office 2013");
+//            DevExpress.Skins.SkinManager.EnableFormSkins();
+//            DevExpress.UserSkins.BonusSkins.Register();
+//            UserLookAndFeel.Default.SetSkinStyle("Office 2013");
 
-            AE_Dev_J.Form.ClassificationForm classform = new Form.ClassificationForm();
-            classform.Show();
+//            AE_Dev_J.Form.ClassificationForm classform = new Form.ClassificationForm();
+//            classform.Show();
 
-            Application.Run();
+//            Application.Run();
             
-        }
-    }
-}
+//        }
+//    }
+//}
