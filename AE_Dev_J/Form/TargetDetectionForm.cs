4ebcc2e65044606ea97fb81f671f0f5bb9b3510d
hengyu123 <hengyu.hengyu@163.com>
2016-04-09 16:33:22 +0800

    reconfig ignore files

---
+++ AE_Dev_J/Form/TargetDetectionForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace AE_Dev_J.Form
+{
+    /// <summary>
+    /// 目标探测窗口
+    /// </summary>
+    public partial class TargetDetectionForm : DevExpress.XtraEditors.XtraForm
+    {
+        public TargetDetectionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
